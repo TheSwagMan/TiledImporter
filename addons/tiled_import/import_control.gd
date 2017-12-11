bc824290e0debc5f3ce5b3f69f05c1904814e4c0
Thomas POTIER <thomas.potier.92@hotmail.fr>
2017-12-11 11:12:41 +0100

    Finished v0.1

--- addons/tiled_import/import_control.gd
+++ addons/tiled_import/import_control.gd
@@ -2,73 +2,117 @@ tool
 extends Control
 
 func _ready():
-	$container/map_btn.connect("pressed", self, "map_ask_path")
-	$container/set_btn.connect("pressed", self, "set_ask_path")
-	$container/ok_btn.connect("pressed", self, "do_it")
-	$container/map_dlg.connect("file_selected", self, "map_path")
-	$container/set_dlg.connect("file_selected", self, "set_path")
+	$tabc/Tileset/container/set_in_btn.connect("pressed", self, "set_import_ask_path")
+	$tabc/Tileset/container/set_out_btn.connect("pressed", self, "set_export_ask_path")
+	$tabc/Tileset/container/ok_btn.connect("pressed", self, "import_tileset")
+	$tabc/Tileset/container/set_in_dlg.connect("file_selected", self, "set_import_path")
+	$tabc/Tileset/container/set_out_dlg.connect("file_selected", self, "set_export_path")
 
-func map_ask_path():
-	$container/map_dlg.popup()
+	$tabc/Tilemap/container/map_in_btn.connect("pressed", self, "map_import_ask_path")
+	$tabc/Tilemap/container/set_in_btn.connect("pressed", self, "set_open_ask_path")
+	$tabc/Tilemap/container/map_out_btn.connect("pressed", self, "map_export_ask_path")
+	$tabc/Tilemap/container/ok_btn.connect("pressed", self, "import_tilemap")
+	$tabc/Tilemap/container/set_in_dlg.connect("file_selected", self, "set_open_path")
+	$tabc/Tilemap/container/map_in_dlg.connect("file_selected", self, "map_import_path")
+	$tabc/Tilemap/container/map_out_dlg.connect("file_selected", self, "map_export_path")
 
-func set_ask_path():
-	$container/set_dlg.popup()
+func set_import_ask_path():
+	$tabc/Tileset/container/set_in_dlg.popup()
 
-func map_path(path):
-	$container/map_pth.text = path
+func set_export_ask_path():
+	$tabc/Tileset/container/set_out_dlg.popup()
 
-func set_path(path):
-	$container/set_pth.text = path
+func set_open_ask_path():
+	$tabc/Tilemap/container/set_in_dlg.popup()
 
-func do_it():
-	var map_file = File.new()
-	var e
-	var root
-	var packed
-	e = map_file.open($container/map_pth.text, File.READ)
-	if e != OK:
-		print("Error :" + str(e))
+func map_import_ask_path():
+	$tabc/Tilemap/container/map_in_dlg.popup()
+
+func map_export_ask_path():
+	$tabc/Tilemap/container/map_out_dlg.popup()
+
+func set_import_path(path):
+	$tabc/Tileset/container/set_in_pth.text = path
+
+func set_export_path(path):
+	$tabc/Tileset/container/set_out_pth.text = path
+
+func set_open_path(path):
+	$tabc/Tilemap/container/set_in_pth.text = path
+
+func map_import_path(path):
+	$tabc/Tilemap/container/map_in_pth.text = path
+
+func map_export_path(path):
+	$tabc/Tilemap/container/map_out_pth.text = path
+
+func parse_my_file(path):
+	var file = File.new()
+	var error = file.open(path, File.READ)
+	if error != OK:
+		print("Error : " + str(error))
 		return FAILED
-	var a = parse_json(map_file.get_as_text())
-	var set_file = File.new()
-	e = set_file.open($container/set_pth.text, File.READ)
-	if e != OK:
-		print("Error :" + str(e))
-		return FAILED
-	var b = parse_json(set_file.get_as_text())
-	# parse tileset
-	var tileset = TileSet.new()
-	root = Node2D.new()
-	root.set_name(b.name)
-	print($container/set_pth.text.get_base_dir() + "/" + b.image)
-	var img = load($container/set_pth.text.get_base_dir() + "/" + b.image).get_data()
-	for i in range(b.columns):
-		for j in range(b.tilecount / b.columns):
-			var idx = i * b.tilecount / b.columns + j
+	var parsed = parse_json(file.get_as_text())
+	file.close()
+	return (parsed)
+	
+
+func import_tileset():
+	print("Importing tileset...")
+	var tileset_path = $tabc/Tileset/container/set_in_pth.text
+	var export_path = $tabc/Tileset/container/set_out_pth.text
+	print("From " + tileset_path + " to " + export_path)
+	
+	var parsed_tileset = parse_my_file(tileset_path)
+	# var tileset = TileSet.new()
+	var root = Node2D.new()
+	root.set_name(parsed_tileset.name)
+	var image_path = tileset_path.get_base_dir() + "/" + parsed_tileset.image
+	# var new_image_path = export_path.get_basename() + "." + image_path.get_extension()
+	var dir = Directory.new()
+	# var error = dir.copy(image_path, new_image_path)
+	# if error != OK:
+	# 	print("Error : " + str(error))
+	# 	return FAILED
+	var img_tex = ImageTexture.new()
+	img_tex.load(image_path)
+	var img = img_tex.get_data()
+	for i in range(parsed_tileset.tilecount / parsed_tileset.columns):
+		for j in range(parsed_tileset.columns):
+			var idx = i * parsed_tileset.tilecount / parsed_tileset.columns + j
 			var sprite = Sprite.new()
+			sprite.set_name(str(idx))
 			var tex = ImageTexture.new()
 			sprite.centered = false
-			sprite.position = Vector2(j * b.tileheight, i * b.tilewidth)
-			tex.create_from_image(img.get_rect(Rect2(sprite.position, Vector2(b.tilewidth, b.tileheight))))
+			sprite.position = Vector2(j * parsed_tileset.tileheight, i * parsed_tileset.tilewidth)
+			tex.create_from_image(img.get_rect(Rect2(sprite.position, Vector2(parsed_tileset.tilewidth, parsed_tileset.tileheight))))
 			tex.set_flags(0)
-			$TextureRect.texture = tex
 			sprite.texture = tex
 			root.add_child(sprite)
 			sprite.set_owner(root)
-			tileset.create_tile(idx)
-			tileset.tile_set_name(idx, ("%0" + str(str(b.tilecount).length()) + "d") % idx)
-			tileset.tile_set_texture(idx, tex)
-	packed = PackedScene.new()
+			# tileset.create_tile(idx)
+			# tileset.tile_set_name(idx, ("%0" + str(str(b.tilecount).length()) + "d") % idx)
+			# tileset.tile_set_texture(idx, tex)
+	var packed = PackedScene.new()
 	packed.pack(root)
-	ResourceSaver.save($container/set_pth.text + ".tileset.tscn", packed)
-	ResourceSaver.save($container/set_pth.text + ".tileset.tres", tileset)
-	# parse tilemap
-	root = Node2D.new()
-	for layer in a.layers:
+	ResourceSaver.save(export_path, packed)
+	# ResourceSaver.save($container/set_pth.text + ".tileset.tres", tileset)
+	print("Done !")
+
+func import_tilemap():
+	print("Importing tilemap...")
+	var tilemap_path = $tabc/Tilemap/container/map_in_pth.text
+	var tileset_path = $tabc/Tilemap/container/set_in_pth.text
+	var export_path = $tabc/Tilemap/container/map_out_pth.text
+	print("From " + tilemap_path + " and " + tileset_path + " to " + export_path)
+	var parsed_tilemap = parse_my_file(tilemap_path)
+	var tileset = load(tileset_path)
+	var root = Node2D.new()
+	for layer in parsed_tilemap.layers:
 		if layer.type == "tilelayer":
 			var tm = TileMap.new()
 			tm.tile_set = tileset
-			tm.cell_size = Vector2(a.tilewidth, a.tileheight)
+			tm.cell_size = Vector2(parsed_tilemap.tilewidth, parsed_tilemap.tileheight)
 			tm.set_name(layer.name)
 			tm.modulate = 0xffffff | (int(0xff * layer.opacity) << 24)
 			tm.visible = layer.visible
@@ -77,7 +121,7 @@ func do_it():
 			for i in range(layer.height):
 				for j in range(layer.width):
 					tm.set_cell(j - layer.y, i - layer.x, layer.data[i * layer.width + j] - 1)
-	packed = PackedScene.new()
+	var packed = PackedScene.new()
 	packed.pack(root)
-	ResourceSaver.save($container/map_pth.text + ".tilemap.tscn", packed)
-	print("Done.")
+	ResourceSaver.save(export_path, packed)
+	print("Done !")
